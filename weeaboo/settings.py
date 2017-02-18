ca46cc849cef168db1fa07026da0e708f00970ff
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2017-02-18 18:06:39 -0500

    Autothrottle

--- weeaboo/settings.py
+++ weeaboo/settings.py
@@ -1,7 +1,10 @@
-# Spiders
+# Spider
 SPIDER_MODULES = ['weeaboo.spiders']
 NEWSPIDER_MODULE = 'weeaboo.spiders'
 
 # Cache
 HTTPCACHE_ENABLED = True
 HTTPCACHE_GZIP = True
+
+# Throttle
+AUTOTHROTTLE_ENABLED = True
