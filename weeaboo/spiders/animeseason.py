3dc418dc113cfe5cf84e1d2ccdd472e09c3cc62b
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2017-02-18 18:19:28 -0500

    animeseason

--- weeaboo/spiders/animeseason.py
+++ weeaboo/spiders/animeseason.py
@@ -10,4 +10,10 @@ class AnimeseasonSpider(scrapy.Spider):
             yield scrapy.Request(response.urljoin(href), self.parse_anime)
 
     def parse_anime(self, response):
-        print(response)
+        yield {}
+
+        for href in response.css('.content_bloc > table td:first-child a::attr("href")').extract():
+            yield scrapy.Request(response.urljoin(href), self.parse_episode)
+
+    def parse_episode(self, response):
+        yield {}
