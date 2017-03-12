daaeafc40e9f2554b69b774479e46afb8a7a9317
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2017-03-12 12:51:20 -0400

    Working storage pipeline

--- weeaboo/pipelines/storage.py
+++ weeaboo/pipelines/storage.py
@@ -1,30 +1,26 @@
 import sqlite3
 
 class StoragePipeline(object):
-    def __init__(self, filename):
-        self.filename = filename
+    def __init__(self, database_url):
+        self.database_url = database_url
 
     @classmethod
     def from_crawler(cls, crawler):
-        return cls (
-            filename = crawler.settings.get('DATABASE_URL')
-        )
+        return cls(database_url=crawler.settings.get('DATABASE_URL'))
 
     def open_spider(self, spider):
-        self.con = sqlite3.connect(self.filename)
+        self.con = sqlite3.connect(self.database_url, uri=True)
         self.cur = self.con.cursor()
 
     def close_spider(self, spider):
         self.con.close()
 
     def process_item(self, item, spider):
-        anime = (
-           item['title'],
-           item['category'],
-           item['status'],
-           item['synopsis'],
-           item['image']
-        )
-        self.cur.execute("INSERT INTO anime VALUES (?, ?, ?, ?, ?)", anime)
+        columns = ', '.join(item.keys())
+        placeholders = ':' + ', :'.join(item.keys())
+        query = 'INSERT INTO anime ({}) VALUES ({})'.format(columns, placeholders)
+
+        self.cur.execute(query, dict(item))
         self.con.commit()
+
         return item
