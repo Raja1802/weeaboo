9c2f713865dfb7ef51c528991641ac50701d9f91
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2017-03-12 12:51:11 -0400

    Better command for database management

--- weeaboo/commands/database.py
+++ weeaboo/commands/database.py
@@ -6,6 +6,7 @@ from scrapy.exceptions import UsageError
 class Command(ScrapyCommand):
 
     requires_project = True
+    default_settings = {'LOG_ENABLED': False}
 
     def syntax(self):
         return '<action>'
@@ -17,17 +18,17 @@ class Command(ScrapyCommand):
         if len(args) != 1:
             raise UsageError()
 
-        con = sqlite3.connect(self.crawler_process.settings['DATABASE_URL'], uri=True)
+        con = sqlite3.connect(self.crawler_process.settings.get('DATABASE_URL'), uri=True)
         cur = con.cursor()
 
         if args[0] == 'create':
             cur.execute('''CREATE TABLE IF NOT EXISTS anime (
-                id integer PRIMARY KEY,
-                title text NOT NULL,
-                category text NOT NULL DEFAULT series,
-                status text,
-                synopsis text,
-                image text
+                id INTEGER PRIMARY KEY,
+                title TEXT NOT NULL,
+                category TEXT NOT NULL DEFAULT series,
+                status TEXT,
+                synopsis TEXT,
+                image TEXT
             )''')
         elif args[0] == 'drop':
             cur.execute('DROP TABLE IF EXISTS anime')
