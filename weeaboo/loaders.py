1a0b19f75c61b00fc37715f1de2dbf4ef96fb757
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2017-03-15 18:27:30 -0400

    Lowercase status and category

--- weeaboo/loaders.py
+++ weeaboo/loaders.py
@@ -6,3 +6,5 @@ from scrapy.loader.processors import MapCompose, TakeFirst
 class AnimeLoader(ItemLoader):
     default_input_processor = MapCompose(remove_tags, strip_html5_whitespace)
     default_output_processor = TakeFirst()
+
+    status_in = category_in = MapCompose(remove_tags, strip_html5_whitespace, str.lower)
